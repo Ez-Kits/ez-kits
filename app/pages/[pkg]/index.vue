3ed4325e6c724f4daefd6b09e6e36c9fb159d411
Pham Van Manh <phammanh.1221998@gmail.com>
2025-05-04 23:44:23 +0700

    feat: redirect route [pkg] to route [pkg]/[version]

---
+++ app/pages/[pkg]/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { getLibraryLatestVersionIndexUrl } from "~/libraries";
+import type { LibraryName } from "~/libraries/libraries";
+
+const route = useRoute("pkg");
+const pkg = route.params.pkg;
+
+await navigateTo(getLibraryLatestVersionIndexUrl(pkg as LibraryName));
+</script>
